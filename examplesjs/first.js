b7cfed0f3fbf70001144df970c52e199842c0f9f
LilianaCondrea <l.cndrea@yahoo.com>
2020-07-15 14:28:48 +0300

    Website boolog

---
+++ examplesjs/first.js
@@ -0,0 +1,179 @@
+
+        // var a=3;
+        // var b=4;
+        // console.log(a+b);
+// var a=10;
+// var b=15;
+// c=0;
+// if(c==0)
+// if(!c)
+// if(c)
+// if(a>b && b==3){
+
+// }
+// if(a>b || b==3){
+
+// }
+
+// if(a>b)
+// {
+// console.log(a);
+// }
+// else if(a!=b)
+// {
+// console.log(0)
+// }
+// else{console.log(b)}
+
+// var i=5;
+// console.log(i--);
+// console.log(--i);
+// console.log(i++);
+// console.log(++i);
+// console.log(--i);
+
+//CICLURI
+//for,while,do while
+//1.while---verifica si apoi executa
+//ex1
+// var i=0;
+// while(i<6)
+// {console.log(i);
+//  i++;}
+//ex2
+// i=10;
+// while(i>1)
+// {
+//         console.log(i);
+//         i--;
+// }
+
+//2.do while---executa si apoi verifica
+//ex1
+// var j=0;
+// do{
+//   console.log(j);
+//   j++;
+// }
+// while(j<6);
+
+
+//3.for
+// var i=0;
+// for(i=0;i<8;i++)
+// {console.log(i);}
+
+//4.verif cite vocale sunt intr-un string
+// var s="cool";
+// console.log(s.length);
+// k=0;
+// for(i=0;i<=s.length;i++)
+// {
+//         if(s[i]=='o') k++;
+// }
+// console.log(k);
+
+
+//5.Arrays-tablouri
+//ex1
+//citim nota la 4 elevi
+// var note=[3,4,5,6];
+// console.log(note[2]) ;
+//ex1.2
+//adunam notele
+// s=0;//suma
+// for(i=0;i<note.length;i++)
+// {
+// s=s+note[i];
+// console.log(s);
+// }
+// console.log(s/4);
+
+//p/u acasa
+// var v=[10,9,-3,6];de gasit min si max
+
+
+// a=6;
+// b=6;
+// (a==b)?console.log(b):console.log(a)
+// if(a==b)
+// {console.log(b);}
+// else{console.log(a);}
+
+// switch(p/u acasa)
+
+// Homework
+// ex1
+// var c='+,-,/,*';
+// a+b a-b a/b a*b
+// var a=10;
+// var b=15;
+// var c=a+b;
+// switch(c)
+// {
+//         case '+':
+//                 console.log(a+b );
+//                 break;
+//         case '-':
+//                 console.log(a-b);
+//                 break;
+//         case '/':
+//                 console.log( a/b);
+//                 break;
+//         case '*':
+//                 console.log(a*b);
+//                 break;
+//            default:;
+// }
+// 
+
+
+// ex2-min,max
+// var x,y,z;
+// x=5;
+// y=6;
+// z=-3;
+
+//ex3
+// var e=6;
+// var t=3;
+// sa se afiseze
+// console.log(e)->3(t)
+// console.log(t) ->6(e)
+
+
+// de citit Array,DOM
+
+
+//Manipularea cu html DOM
+// console.log(document);
+// document.getElementById('a').innerHTML='Lalalala';
+
+
+
+//Functions JS
+//1ex
+// var a=3;
+// var b=5;
+// var c=6;
+// function calc(x)
+// {
+// if(x<0){console.log("neg");}else{console.log("pos");}
+// }
+//apelarea functiei calc
+// calc(a);
+// calc(b);
+// calc(-3);
+
+// function calc(a,b){
+//         return a+b;
+// }
+// var s=calc(3,4);
+
+
+
+//js events
+function clic()
+{
+        alert("Hi");
+}
